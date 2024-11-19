5066f41bc6cb06c7be9014ef60ba82d4ddd4199d
kopami <khoipham2310@gmail.com>
2024-11-19 11:11:10 +0700

    [KhoiPM][Dev]: Change Product entity attribute

--- Ecommerce.ProductApiSolution/ProductApi.Domain/Entities/Product.cs
+++ Ecommerce.ProductApiSolution/ProductApi.Domain/Entities/Product.cs
@@ -1,14 +1,17 @@
-﻿using System;
+﻿using eCommerce.ShareLibrary.Abstract;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProductApi.Domain.Entities
 {
-    public class Product
+    public class Product : BaseEntity
     {
-        public int Id { get; set; }
+        [Key]
+        public override Guid Id { get; set; }
         public string? Name { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
