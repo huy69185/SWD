68ef682354d84feae3ed27f317048578b213fb64
huy69185 <huyh69185@gmail.com>
2024-11-20 14:08:40 +0700

    Create DTO

---
+++ Ecommerce.OrderApiSolution/OrderApi.Application/DTOs/OrderDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OrderApi.Application.DTOs
+{
+    public record OrderDTO(
+        int Id,
+
+        [Required(ErrorMessage = "Product ID is required.")]
+        string ProductId,
+
+        [Required, Range(1, int.MaxValue, ErrorMessage = "Client ID must be at least 1.")]
+        int ClientId,
+
+        [Required, Range(1, int.MaxValue, ErrorMessage = "Purchase quantity must be at least 1.")]
+        int PurchaseQuantity,
+
+        [Required(ErrorMessage = "Order date is required.")]
+        DateTime OrderDate
+    );
+}
---
+++ Ecommerce.OrderApiSolution/OrderApi.Application/DTOs/OrderDetailsDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderApi.Application.DTOs
+{
+    public record OrderDetailsDTO(
+        [Required]int OrderId,
+        [Required] string ProductId,
+        [Required] int ClientId,
+        [Required, EmailAddress] string Email,
+        [Required] string TelephoneNumber,
+        [Required] string ProductName,
+        [Required] int PurchaseQuantity,
+        [Required, DataType(DataType.Currency)] decimal UnitPrice,
+        [Required, DataType(DataType.Currency)] decimal TotalPrice,
+        [Required] DateTime OrderedDate
+        );   
+}
