4b27fe3328a072caa58d8f0392569dd18d349e32
Nguyễn Minh Khôi Nguyên <nguyennmkse170356@gmail.com>
2025-03-12 07:08:25 +0700

    Child Demo

---
+++ GrowthTracking.ChildApiSolution/ChildApi.Application/DTOs/MilestoneDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ChildApi.Application.DTOs
+{
+    // DTO cho Milestone.
+    public record MilestoneDTO(
+        Guid? Id,        // Nullable cho record mới
+        Guid ChildId,
+        string MilestoneType,
+        DateTime MilestoneDate,
+        string? Description
+    );
+}
---
+++ GrowthTracking.ChildApiSolution/ChildApi.Application/Interfaces/IChildRepository.cs
@@ -0,0 +1,20 @@
+﻿using ChildApi.Application.DTOs;
+using GrowthTracking.ShareLibrary.Response;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ChildApi.Application.Interfaces
+{
+    public interface IChildRepository
+    {
+        Task<Response> CreateChildAsync(ChildDTO childDto);
+        Task<Response> UpdateChildAsync(ChildDTO childDto);
+        Task<ChildDTO?> GetChildAsync(Guid childId);
+        Task<IEnumerable<ChildDTO>> GetChildrenByParentAsync(Guid parentId);
+        Task<Response> DeleteChildAsync(Guid childId);
+
+        // Tính toán BMI dựa trên cân nặng và chiều cao
+        decimal? CalculateBMI(ChildDTO childDto);
+    }
+}
