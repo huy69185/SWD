2f368c3f6e4d15066af1625b69ccdba7d5a24481
huy69185 <huyh69185@gmail.com>
2025-03-26 18:59:24 +0700

    add IScheduleRepository.cs

---
+++ GrowthTracking.BookingManagementAPISolution/BookingApi.Application/Interfaces/IScheduleRepository.cs
@@ -0,0 +1,18 @@
+﻿using BookingApi.Application.DTOs;
+using GrowthTracking.ShareLibrary.Response;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookingApi.Application.Interfaces
+{
+    public interface IScheduleRepository
+    {
+        Task<Response> CreateScheduleAsync(ScheduleDTO scheduleDto);
+        Task<Response> UpdateScheduleAsync(ScheduleDTO scheduleDto);
+        Task<ScheduleDTO?> GetScheduleAsync(Guid scheduleId);
+        Task<IEnumerable<ScheduleDTO>> GetSchedulesByDoctorAsync(Guid doctorId, DateTime? startDate, DateTime? endDate);
+        Task<Response> DeleteScheduleAsync(Guid scheduleId);
+        Task<bool> CheckDoctorScheduleConflictAsync(Guid doctorId, DateTime startTime, DateTime endTime, Guid scheduleId = default);
+    }
+}
