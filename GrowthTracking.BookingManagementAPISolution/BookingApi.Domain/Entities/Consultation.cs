ae27bf0f12f41d57584a2c4629d6e55a3fae1bfc
huy69185 <huyh69185@gmail.com>
2025-03-26 18:54:51 +0700

    add consultation

---
+++ GrowthTracking.BookingManagementAPISolution/BookingApi.Domain/Entities/Consultation.cs
@@ -0,0 +1,18 @@
+﻿using GrowthTracking.ShareLibrary.Abstract;
+using System;
+
+namespace BookingApi.Domain.Entities
+{
+    public class Consultation : BaseEntity
+    {
+        public override Guid Id { get; set; }
+        public Guid BookingId { get; set; }
+        public Guid DoctorId { get; set; }
+        public DateTime ConsultationDate { get; set; }
+        public string? ConsultationNotes { get; set; }
+        public string Status { get; set; } = "scheduled";
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public bool StatusDelete { get; set; } = false; 
+    }
+}
