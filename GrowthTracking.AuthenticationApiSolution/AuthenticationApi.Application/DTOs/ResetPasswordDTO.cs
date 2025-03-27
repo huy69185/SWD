e4dc2a7d84f989a7d1f039a78d75aae825111763
huy69185 <huyh69185@gmail.com>
2025-03-27 16:19:47 +0700

    Update validate password

---
+++ GrowthTracking.AuthenticationApiSolution/AuthenticationApi.Application/DTOs/ResetPasswordDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationApi.Application.DTOs
+{
+    public record ResetPasswordDTO(
+        [Required] string Code, 
+        [Required] string OldPassword,
+        [Required, MinLength(8)] string NewPassword 
+    );
+}
